ae76856bcaf14582b84638dede0cd4d3c13975fd
tututuu <1002926288@qq.com>
2018-08-30 14:59:37 +0800

    修改了mysql密码

--- source/application/database.php
+++ source/application/database.php
@@ -6,7 +6,7 @@ $config = [
     'host' => '127.0.0.1',
     'database' => 'we7_cn',
     'username' => 'root',
-    'password' => 'admin',
+    'password' => 'a',
     'port' => '3306',
     'charset' => 'utf8',
 ];
